ddcb1330e4c90eab4f1ca43f017fb5458d9b6828
r.tatarinov <r.tatarinov@taxys.ru>
2018-04-10 16:05:20 +0300

    ...

--- Business/Entities/Order.cs
+++ Business/Entities/Order.cs
@@ -35,8 +35,11 @@ namespace Business.Entities
             comments.Add(comment);
         }        
 
-        internal static string ThrowIfNullAddress(string address) =>
-            address ?? throw new ArgumentNullException(nameof(address));
+        internal static void ThrowIfNullAddress(string address)
+        {
+            if (address == null)
+                throw new ArgumentNullException(nameof(address));
+        }
 
         internal class Map
         {
@@ -47,6 +50,9 @@ namespace Business.Entities
 
         public static Order CreatePresent(string beginAddress, string endAddress, string commentMessage, DateTimeOffset createdAt)
         {
+            ThrowIfNullAddress(beginAddress);
+            ThrowIfNullAddress(endAddress);
+
             var order = new Order(new OrderData
             {
                 BeginAddress = beginAddress,
