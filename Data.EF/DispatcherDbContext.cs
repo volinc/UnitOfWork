edd5e736a627c3a95635fa9bda9b24f272f923d7
r.tatarinov <r.tatarinov@taxys.ru>
2020-01-19 01:46:19 +0300

    ...

--- Data.EF/DispatcherDbContext.cs
+++ Data.EF/DispatcherDbContext.cs
@@ -1,26 +1,23 @@
 ﻿using Business.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Data.EF
 {
     public class DispatcherDbContext : DbContext
     {
-        public DbSet<OrderData> Orders { get; set; }
-        
-        public DbSet<SuggestionData> Suggestions { get; set; }
-
-        public DbSet<ShiftData> Shifts { get; set; }
-
-        public DbSet<VehicleData> Vehicles { get; set; }
-
-        public DbSet<DriverData> Drivers { get; set; }
+        public static readonly ILoggerFactory LoggerFactory =
+            Microsoft.Extensions.Logging.LoggerFactory.Create(builder => { builder.AddDebug().AddConsole(); });
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 var connectionString = @"Server=.; Database=test-db-dispatcher; User Id=sa; Password=12345(!)a;";
-                optionsBuilder.UseSqlServer(connectionString, providerOptions =>
+                optionsBuilder
+                    .UseLoggerFactory(LoggerFactory)
+                    .EnableSensitiveDataLogging()
+                    .UseSqlServer(connectionString, providerOptions =>
                 {
                     providerOptions.EnableRetryOnFailure();
                 });                
@@ -31,5 +28,16 @@ namespace Data.EF
         {
             modelBuilder.Entity<OrderCommentData>().ToTable("OrderComments");
         }
+
+        public DbSet<OrderData> Orders { get; set; }
+
+        public DbSet<SuggestionData> Suggestions { get; set; }
+
+        public DbSet<ShiftData> Shifts { get; set; }
+
+        public DbSet<VehicleData> Vehicles { get; set; }
+
+        public DbSet<DriverData> Drivers { get; set; }
+
     }
 }
